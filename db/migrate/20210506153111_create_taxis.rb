c5fee36e8faf83a5d506b7085abbbef7ee51cdc6
thadeus <thadeusnyariki94@gmail.com>
2022-11-25 21:48:19 +0300

    Done assignment

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.string  :driver_name
+      t.string  :car_plate
+      t.string  :made
       t.timestamps
     end
   end
